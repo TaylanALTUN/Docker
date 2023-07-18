7960124ed0e79160cae9845b7e9f0f6b6cc7644c
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-18 07:59:00 +0300

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 1;
+while (i<10000)
+{
+    Console.WriteLine($"Hello, World! {i}");
+    i++;
+
+    Thread.Sleep(1000);
+}
+
